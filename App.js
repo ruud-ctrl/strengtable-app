05041268b4dde4eb60735cce6f92574411290519
ruud-ctrl <me@ruudjuffermans.nl>
2025-11-13 19:00:43 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text style={{color: "red"}}>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
